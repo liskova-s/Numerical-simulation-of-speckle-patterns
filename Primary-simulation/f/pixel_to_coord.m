f5ee0cf6e1d03f7be8235ee0fb9490e8c8eb5604
liskova-s <79690997+liskova-s@users.noreply.github.com>
2023-02-10 19:43:57 +0100

    Delete pixel_to_coord.m

--- Primary-simulation/f/pixel_to_coord.m
+++
@@ -1,7 +0,0 @@
-function [cx,cy] = pixel_to_coord(pxx,pxy,slm_pixel,slm_resolution)
-    % returns the coordinates of center of given pixel
-    x1 = slm_resolution(1)/2*slm_pixel-slm_pixel/2;
-    y1 = -slm_resolution(2)/2*slm_pixel + slm_pixel/2;
-    cx = -(pxx-1)*slm_pixel+x1;
-    cy = (pxy-1)*slm_pixel + y1;
-end
