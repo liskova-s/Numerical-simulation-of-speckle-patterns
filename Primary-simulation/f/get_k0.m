b4d9e02eaeb14119c9bb1e8453ea347a53aaf114
liskova-s <79690997+liskova-s@users.noreply.github.com>
2023-04-15 17:26:19 +0200

    delete

--- Primary-simulation/f/get_k0.m
+++
@@ -1,6 +0,0 @@
-function k1 = get_k0(L_coord, beam_center,lambda, slm_pixel,slm_resolution)
-        [bcx,bcy] = pixel_to_coord(beam_center(1),beam_center(2),slm_pixel,slm_resolution);
-        k1 = [bcx-L_coord(1)-slm_pixel/2,bcy-L_coord(2)+slm_pixel/2,0-L_coord(3)];
-        k1 = k1./norm(k1)*2*pi/lambda;
-end
-
