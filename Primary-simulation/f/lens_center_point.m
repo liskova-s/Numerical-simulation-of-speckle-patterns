c4d0441c2016910a8262385fb3055242d81a981c
liskova-s <79690997+liskova-s@users.noreply.github.com>
2023-04-15 17:23:30 +0200

    delete

--- Primary-simulation/f/lens_center_point.m
+++
@@ -1,7 +0,0 @@
-function lc = lens_center_point(k,z, c1,c2)
-        % assuming the lens is perfectly vertically oriented
-        t = z/k(3);
-        x = c1 + t*k(1);
-        y = c2 + t*k(2);
-        lc = [x,y,z];
-    end
